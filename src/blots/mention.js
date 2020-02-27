cbda0d27ff7501605a054d1ddbff010b2b86de15
Michael Pitzer <mail@mpitzer.at>
2020-02-27 23:04:49 +0100

    Remove mention element

--- src/blots/mention.js
+++ src/blots/mention.js
@@ -5,20 +5,11 @@ const Embed = Quill.import("blots/embed");
 class MentionBlot extends Embed {
   static create(data) {
     const node = super.create();
-    const denotationChar = document.createElement("span");
-    denotationChar.className = "ql-mention-denotation-char";
-    denotationChar.innerHTML = data.denotationChar;
-    node.appendChild(denotationChar);
-    node.innerHTML += data.value;
     return MentionBlot.setDataValues(node, data);
   }
 
   static setDataValues(element, data) {
-    const domNode = element;
-    Object.keys(data).forEach(key => {
-      domNode.dataset[key] = data[key];
-    });
-    return domNode;
+    return element;
   }
 
   static value(domNode) {
